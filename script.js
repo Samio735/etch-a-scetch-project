e756774e2a9051e7e066ef9ca57f0b0363d3099f
Samy <s_rahim@estin.dz>
2022-12-24 03:01:06 +0100

    add coloring

--- script.js
+++ script.js
@@ -6,10 +6,21 @@ ndivs = 16
 function createDiv(clsname){
     const div = document.createElement("div")
     div.className = clsname
+    div.addEventListener("click", () => {
+        div.setAttribute("style","background-color:#ff2563")
+    })
     return div
 }
-let style = "    grid-template-columns: "
+
+
+
 function start(){
+    let style = "    grid-template-columns: "
+    
+    while ( container.hasChildNodes()){
+        container.removeChild(container.firstChild)
+}
+
     for( let i =0 ; i< ndivs;i++){
         style = style + " 1fr"
         console.log(style)
@@ -21,8 +32,12 @@ function start(){
 }
 }
 
-
- ndivs = prompt("enter number of squares per side")
+const btn = document.querySelector(".change")
+btn.addEventListener("click", () => {
+    ndivs = prompt("enter number of squares per side")
+    start()
+})
+ 
 
  start()
 
